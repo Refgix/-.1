d45023bd68fd1596773a401c8c26a381ebd313ca
Refgix <153459900+Refgix@users.noreply.github.com>
2023-12-10 23:44:54 +0300

    Добавьте файлы проекта.

---
+++ Переменные ДЗ1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Переменные_ДЗ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int health;
+            byte armor;
+            bool test;
+            float resistance;
+            short exp;
+            string dialogue;
+            char rank;
+            long money;
+            ulong damage;
+            uint endurance;
+
+        }
+    }
+}
